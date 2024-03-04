405e3b45c4f4cecf05c3a22874b199b0ecfa0075
Daniel Maclaren <danieltmaclaren@gmail.com>
2024-03-04 18:05:18 +0000

    change the idea of the project to be a simple routine tracker to be used at the gym, altered files and added new .html files

--- app.py
+++ app.py
@@ -7,5 +7,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-    if __name__ == "__main__":
-        app.run(debug=True)
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
+
+@app.route("/training")
+def training():
+    return render_template("training.html")
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
