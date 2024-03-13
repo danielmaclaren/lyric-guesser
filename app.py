5f9f6cd0ac29b941884b7fcf242069d3dd1e0f62
Daniel Maclaren <danieltmaclaren@gmail.com>
2024-03-13 17:29:39 +0000

    started working on the log in and register options

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from helpers import login_required
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+
+db = sqlite3.connect('workout.db')
+
 
 app = Flask(__name__)
 
@@ -13,17 +19,54 @@ def login():
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    """Register user"""
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords must match", 400)
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        if len(rows) >= 1:
+            return apology(
+                "username is already taken, please use another username", 400
+            )
+        else:
+            hash_password = generate_password_hash(request.form.get("password"))
+
+            new_user = db.execute(
+                "INSERT INTO users (username, hash) VALUES (?, ?)",
+                request.form.get("username"),
+                hash_password
+            )
+
+        session["user_id"] = new_user
+
+        return redirect("/")
+
+    elif request.method == "GET":
+        return render_template("register.html")
 
 @app.route("/aboutme")
+@login_required
 def aboutme():
     return render_template("aboutme.html")
 
 @app.route("/training")
+@login_required
 def training():
     return render_template("training.html")
 
 @app.route("/history")
+@login_required
 def history():
     return render_template("history.html")
 
