b1005e284c54aeb770a1344dc16a2fc4bfd91534
Daniel Maclaren <danieltmaclaren@gmail.com>
2024-04-20 15:03:47 +0100

    rep counter now works

--- static/styles/js/training.js
+++ static/styles/js/training.js
@@ -1,16 +1,60 @@
-let count = 1;
+let exercisecount = 1;
+let set = { value: 1 };
+let table = { value: 0};
+let dict = {};
 
-function addSet() {  
-    let set = count;
-    let weight = document.getElementById("weight").value;
-    let reps = document.getElementById("reps").value;
 
-    document.getElementById("setrow").innerHTML += `
+function addSet(tablenumber) {
+    if (table.value != tablenumber) {
+        if (dict[tablenumber] == undefined) {
+            set.value = 1;
+            table.value = tablenumber;
+            dict[tablenumber] = set.value;
+        } else {
+            set.value = dict[tablenumber];
+        }
+    } else {
+        set.value = dict[tablenumber];
+    }
+    
+    
+
+    let weight = document.getElementById("weight"+ tablenumber).value;
+    let reps = document.getElementById("reps" + tablenumber).value;
+
+    document.getElementById("setrow" + tablenumber).innerHTML += `
                 <tr>
-                    <td>${set}</td>
+                    <td id="set${set.value}">${set.value}</td>
                     <td>${weight}</td>
                     <td>${reps}</td>
                 </tr> `;
-    
-    count++;
+    set.value++;
+    dict[tablenumber] = set.value;
+}
+
+function addExercise() {
+    document.getElementById("settable").innerHTML += `
+    <table id="setexercise${exercisecount}" class="table table-hover">
+        <thead>
+            <input type="hidden" name="exercisenumber" id="exercisenumber" value="${exercisecount}">
+            <h2>*Exercise*</h2>
+            <tr>
+                <th>Set</th>
+                <th>Weight</th>
+                <th>Reps</th>
+            </tr>
+        </thead>
+        <tbody id="setrow${exercisecount}">    
+        </tbody>
+        <tfooter>
+            <tr>
+                <td></td>
+                <td><input type="text" name="weight" id="weight${exercisecount}"></td>
+                <td><input type="text" name="reps" id="reps${exercisecount}"></td>
+                <td><input type="button" value="Add Set" + onclick="addSet(${exercisecount})" id="addset${exercisecount}"></td>
+            </tr>
+        </tfooter>
+    </table>
+    `;
+    exercisecount ++;
 }
